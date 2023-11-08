ec992390bec8ee84d6f96f6b15155c9c2b2018e0
rodavok <rodavok@banana-laptop>
2023-11-08 14:04:43 -0500

    imports

---
+++ first_dag.py
@@ -0,0 +1,8 @@
+import json
+import pathlib
+import airflow
+import requests
+import requests.exceptions as requests_exceptions
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator
